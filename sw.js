3c692bd6b001b665362457c116b4546cc58ec404
uelitonscs-netizen <uelitonscs@gmail.com>
2026-04-23 23:01:22 -0400

    New SW cache v20260423z

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fieldbid-v20260423h';
+const CACHE_NAME = 'fieldbid-v20260423z';
 const ASSETS = ['/Green-Idea-Fieldbid/','/Green-Idea-Fieldbid/index.html','/Green-Idea-Fieldbid/app.html','/Green-Idea-Fieldbid/manifest.json','/Green-Idea-Fieldbid/icon-192.png','/Green-Idea-Fieldbid/icon-512.png'];
 self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(function(c){return c.addAll(ASSETS).catch(function(){});}));});
 self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE_NAME;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
