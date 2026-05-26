abf8e97aa9bc027c87644768c4c6f48ca169aa7f
uelitonscs-netizen <uelitonscs@gmail.com>
2026-05-25 23:01:49 -0400

    feat: FieldBid service worker v3

--- sw.js
+++ sw.js
@@ -1,5 +1,15 @@
-const CACHE_NAME = 'fieldbid-v20260423y';
-const ASSETS = ['/Green-Idea-Fieldbid/','/Green-Idea-Fieldbid/index.html','/Green-Idea-Fieldbid/app.html','/Green-Idea-Fieldbid/manifest.json','/Green-Idea-Fieldbid/icon-192.png','/Green-Idea-Fieldbid/icon-512.png'];
-self.addEventListener('install',function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE_NAME).then(function(c){return c.addAll(ASSETS).catch(function(){});}));});
-self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(keys){return Promise.all(keys.filter(function(k){return k!==CACHE_NAME;}).map(function(k){return caches.delete(k);}));}).then(function(){return self.clients.claim();}));});
-self.addEventListener('fetch',function(e){e.respondWith(fetch(e.request).then(function(r){if(r&&r.status===200){var c=r.clone();caches.open(CACHE_NAME).then(function(cache){cache.put(e.request,c);});}return r;}).catch(function(){return caches.match(e.request);}));});
+const CACHE_NAME = 'fieldbid-v3';
+const urlsToCache = ['./', './index.html'];
+self.addEventListener('install', function(e) {
+  e.waitUntil(caches.open(CACHE_NAME).then(function(c) { return c.addAll(urlsToCache); }));
+  self.skipWaiting();
+});
+self.addEventListener('activate', function(e) {
+  e.waitUntil(caches.keys().then(function(keys) {
+    return Promise.all(keys.filter(function(k) { return k !== CACHE_NAME; }).map(function(k) { return caches.delete(k); }));
+  }));
+  self.clients.claim();
+});
+self.addEventListener('fetch', function(e) {
+  e.respondWith(fetch(e.request).catch(function() { return caches.match(e.request); }));
+});
